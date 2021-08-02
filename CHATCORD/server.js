e69c14d0a6d962b25668a925840f57338777ed0a
NaMooJoon <47983023+NaMooJoon@users.noreply.github.com>
2021-08-02 23:06:48 +0900

    Connect database

--- CHATCORD/server.js
+++ CHATCORD/server.js
@@ -2,7 +2,9 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const session = require('express-session');
 const formatMessage = require('./utils/messages');
+const db = require('./utils/db');
 const { 
     userJoin, 
     getCurrentUser, 
@@ -14,20 +16,29 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// Set express-session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'ChatCord Bot';
+const botName = 'Chat Bot';
 
 // Run when client connects
 io.on('connection', socket=> {
+    console.log('새로운 소켓 : ', socket.id);
     socket.on('joinRoom', ({ username, room }) => {
         const user = userJoin(socket.id, username, room);
 
         socket.join(user.room);
 
         // Welcome current user
-        socket.emit('message', formatMessage(botName , 'Welcome to ChatCord!'));
+        socket.emit('message', formatMessage(botName , `Welcome ${user.username}!`));
 
         // Broadcast when a user connects
         socket.broadcast
